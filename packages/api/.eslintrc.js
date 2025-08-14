83e23da66d2320f1c0f5f77e70f9345921f31328
__filename <filename@inft.kr>
2025-08-14 22:07:24 +0800

    feat: Remove links feature and deprecated API package

--- packages/api/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ['@form/eslint-config/nest.js'],
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-};
